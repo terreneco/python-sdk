d3df66e8cdfa87b3be72dcaede9ba2eb3a654033
kpourdeilami <khashayar@terrene.co>
2017-02-26 22:23:18 -0500

    fixed pep8 error

--- src/core/apps.py
+++ src/core/apps.py
@@ -4,6 +4,7 @@ from core.api import APIResourceManagementClient, APIResource
 class Service(APIResource):
     pass
 
+
 class ServiceManagementClient(APIResourceManagementClient):
 
     _resource_class = Service
