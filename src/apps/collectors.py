cb6c8186affbc36b7639dc22e6e575a6a949eccc
kpourdeilami <khashayar@terrene.co>
2017-03-06 20:17:33 -0500

    added write support

--- src/apps/collectors.py
+++ src/apps/collectors.py
@@ -1,18 +1,24 @@
 import requests
 from core.apps import ServiceManagementClient, Service
 from utils.exceptions import NotAuthorizedError, NotFoundError, UnknownError
+from azure.servicebus import ServiceBusService
 
 
 class Collector(Service):
 
-    _access_key = None
-    _connection_string = None
+    _access_key_value = None
+    _access_key_name = None
+    _access_namespace = None
+    _hub = None
 
-    def write(self, data):
+    _service_bus = None
+
+    def write(self, data, hub=None):
         """
         Args:
             data: a dictionary of data to write to
                 the collector
+            hub: the hub to write to
 
         Returns:
             None
@@ -24,26 +30,51 @@ class Collector(Service):
                 could not be found
             UnknownError: if an unexpected error happened
         """
-        pass
+        if hub is None:
+            hub = self._hub
+
+        if self._service_bus:
+            self._service_bus.send_event(
+                hub_name=hub,
+                message=data
+            )
+        else:
+            raise NotFoundError()
 
     def _setup(self):
         """
         Returns: None
         """
-        if self._access_key is None or self._connection_string is None:
+        if None in [
+            self._access_namespace,
+            self._access_key_name,
+            self._access_key_value,
+            self._hub
+        ]:
             self._retrieve_keys()
+
+        if self._service_bus is None:
+            self._service_bus = ServiceBusService(
+                service_namespace=self._access_namespace,
+                shared_access_key_name=self._access_key_name,
+                shared_access_key_value=self._access_key_value
+            )
 
     def _retrieve_keys(self):
         """
         Returns: None
         """
         res = requests.get(
-            self._path + "/keys/",
+            self._path + "keys/",
             headers=self._headers
         )
         if res.status_code == 200:
-            self._connection_string = res.json().get('connection_string')
-            self._access_key = res.json().get('key')
+            self._access_key_name = res.json().get('key_name')
+            self._access_key_value = res.json().get('key_value')
+
+            self._access_namespace = res.json().get('namespace')
+
+            self._hub = res.json().get('hub')
 
 
 class CollectorManagementClient(ServiceManagementClient):
