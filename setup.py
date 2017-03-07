c15b0a90ddc7ca554ca11e156877cbe2d72864ed
kpourdeilami <khashayar@terrene.co>
2017-03-06 20:39:42 -0500

    added setup.py

---
+++ setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+from codecs import open
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with open('requirements.txt') as requirements_file:
+    requirements = requirements_file.readlines()
+
+setup(
+    name="Terrene",
+    version="0.0.1",
+    description="Terrene's Python SDK",
+    url="https://docs.terrene.co",
+    author="Terrene",
+    author_email="developers@terrene.co",
+    license='MIT',
+    keywords="terrene",
+    install_requires=requirements[1:],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+
+        'License :: OSI Approved :: MIT License',
+
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ]
+)
