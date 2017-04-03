e16f42db7f2eaeaef25e90c3c7a640ba6631e2fb
kpourdeilami <khashayar@terrene.co>
2017-04-03 16:39:57 -0400

    WIP test

--- src/test.py
+++ src/test.py
@@ -1,13 +1,51 @@
 from core.auth import UserPassCredentials
 from apps.collectors import CollectorManagementClient
+from apps.storage import StorageManagementClient
 import os
 
 creds = UserPassCredentials(
     username=os.environ.get('USERNAME'),
     password=os.environ.get('PASSWORD')
 )
+print(creds)
+
 
 collector_management_client = CollectorManagementClient(creds)
-collector_management_client.list()[0].write({
-    "message": "hello world"
+collector = collector_management_client.list({
+    "owners__email": os.environ.get('USERNAME')
 })
+
+storage_management_client = StorageManagementClient(creds)
+print(storage_management_client)
+
+# storage_management_client.create({
+#     "name"    : "my stuff",
+#     "contributors": [],
+#     "owners": [os.environ.get('USERNAME')],
+#     "region": "eastus"
+# })
+
+storage = storage_management_client.list({
+    "name": "my stuff"
+})[0]
+print(storage)
+
+storage.create_table("hello")
+# storage.insert_record(
+#     {
+#         "name": "kash"
+#     },
+#     table_name="hello"
+# )
+query = storage.query(
+    filter_str="",
+    table_name="hello"
+)
+print(query)
+
+obj = query[0]
+# obj.partial_update({
+#     "name": "khashayar"
+# })
+print(obj)
+# storage.drop_table("hello")
