5dc6df1e908e50836a5d475624f836476a1f3d87
kpourdeilami <khashayar@terrene.co>
2017-04-20 13:45:33 -0400

    feature to be able to modify the conf module during runtime

--- terrene/conf.py
+++ terrene/conf.py
@@ -5,20 +5,30 @@ Configures the SDK
 import os
 import ast
 
-GUI_SERVER = {
-    "host": "portal.terrene.co",
-    "port": 443,
-    "protocol": "https"
-}
 
-API_SERVER = {
-    "host": "api.terrene.co",
-    "port": 443,
-    "protocol": "https"
-}
+class Terrene:
 
-if os.environ.get('GUI_SERVER', None) is not None:
-    GUI_SERVER = ast.literal_eval(os.environ.get('GUI_SERVER', None))
+    gui_server = {
+        "host": "portal.terrene.co",
+        "port": 443,
+        "protocol": "https"
+    }
+    api_server = {
+        "host": "api.terrene.co",
+        "port": 443,
+        "protocol": "https"
+    }
 
-if os.environ.get('API_SERVER', None) is not None:
-    API_SERVER = ast.literal_eval(os.environ.get('API_SERVER', None))
+    def __init__(self):
+        if os.environ.get('GUI_SERVER', None) is not None:
+            self.gui_server = ast.literal_eval(
+                os.environ.get('GUI_SERVER', None))
+
+        if os.environ.get('API_SERVER', None) is not None:
+            self.api_server = ast.literal_eval(
+                os.environ.get('API_SERVER', None))
+
+    def configure(self, attr, val):
+        setattr(self, attr, val)
+
+terrene = Terrene()
