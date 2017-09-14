beaf158cb119f30eccab3320ac01c07e02935620
kpourdeilami <kpourdeilami@gmail.com>
2017-09-13 23:44:50 -0400

    SDK configuration module

---
+++ terrene/config.py
@@ -0,0 +1,21 @@
+"""
+Configures the SDK
+"""
+import os
+
+
+class URL:
+    def __init__(self, protocol: str, host: str, port: int):
+        self.protocol, self.host, self.port = protocol, host, port
+
+    def __call__(self, *args, **kwargs):
+        return '{}://{}:{}'.format(self.protocol, self.host, self.port)
+
+api = URL(
+    os.environ.get('API_PROTOCOL', 'https'),
+    os.environ.get('API_HOST', 'api.terrene.co'),
+    os.environ.get('API_PORT', 443))
+portal = URL(
+    os.environ.get('API_PROTOCOL', 'https'),
+    os.environ.get('API_HOST', 'portal.terrene.co'),
+    os.environ.get('API_PORT', 443))
