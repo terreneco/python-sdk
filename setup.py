834a5c953ece68fbf713807e9315df100cd4d7b8
kpourdeilami <kpourdeilami@gmail.com>
2017-09-13 23:42:38 -0400

    removed python 2 support and added 3.6

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('requirements.txt') as f:
 
 setup(
     name="Terrene",
-    version="0.0.4",
+    version="0.0.5",
     description="Terrene's Python SDK",
     url="https://docs.terrene.co",
     author="Terrene",
@@ -14,7 +14,7 @@ setup(
     license='MIT',
     keywords="terrene",
     packages=find_packages(exclude=["test"]),
-    install_requires=required[1:],
+    install_requires=required,
     classifiers=[
         'Development Status :: 3 - Alpha',
 
@@ -23,11 +23,10 @@ setup(
 
         'License :: OSI Approved :: MIT License',
 
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6'
     ]
 )
