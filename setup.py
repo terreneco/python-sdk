2f80a066956b028b943dd05add414008912e8926
kpourdeilami <kpourdeilami@gmail.com>
2017-09-27 13:43:21 -0400

    updated the version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('requirements.txt') as f:
 
 setup(
     name="Terrene",
-    version="0.0.5",
+    version="0.0.6",
     description="Terrene's Python SDK",
     url="https://docs.terrene.co",
     author="Terrene",
