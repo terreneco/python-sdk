a86d7e9720f32729ba4569bb0dc4d95a09703bd6
kpourdeilami <kpourdeilami@gmail.com>
2017-10-16 14:44:22 -0400

    increased the version for setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('requirements.txt') as f:
 
 setup(
     name="Terrene",
-    version="0.0.7",
+    version="0.0.8",
     description="Terrene's Python SDK",
     url="https://docs.terrene.co",
     author="Terrene",
