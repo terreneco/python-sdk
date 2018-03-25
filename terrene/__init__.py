96294fb41b23b2d13378d2fc7e78b516c78d44fe
kpourdeilami <me@kpourdeilami.me>
2018-03-25 13:36:00 -0400

    removes pre-set access token

--- terrene/__init__.py
+++ terrene/__init__.py
@@ -1,10 +1,5 @@
-import os
-
 api_protocol = 'https'
 api_host = 'api.terrene.co'
 api_port = 443
 
-access_key = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoiMTJlMjkyYzEtZGI1OC00MmNkLWIxMTEtOWExNDAyNmZmY2EyIiwi' \
-             'dXNlcm5hbWUiOiJlbm9jaHRhbmdAdGVycmVuZS5jbyIsIm9yaWdfaWF0IjoxNTIxNjQzOTY4LCJleHAiOjE4MzcyNjMxNjh9.V9kXOzZk' \
-             '0gRI5tRv8dVTw5Mx_5wH6pJdakemNVnWO80'
-
+access_token = None
