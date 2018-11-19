48f9be79cb867e0558ae4a611f09c2db8a609466
kpourdeilami <k@pourdeilami.com>
2018-11-19 11:06:40 -0500

    bumps the version to 2.0.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="Terrene",
-    version="2.0.2",
+    version="2.0.3",
     description="Terrene's Python SDK",
     url="https://docs.terrene.co",
     author="Terrene",
